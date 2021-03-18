26bd4fd2a75b42bea6280b6b59197c079beedc4e
b6320502347 <78533381+b6320502347@users.noreply.github.com>
2021-03-18 15:59:33 +0700

    .

--- 6320502347_6_1.c
+++ 6320502347_6_1.c
@@ -4,18 +4,27 @@
     int n,i,j;
     scanf("%d",&n);
     char word[n][1000],x[1000];
+    int nub[n],k;
     for(i=0;i<n;i++){
         scanf("%s",word[i]);
         fflush(stdin);
         }
-    for(i=1;i<n;i++)
-    for(j=1;j<n;j++){
-        if(word[j][0]<word[j-1][0]){
-            strcpy(x,word[j-1]);
-            strcpy(word[j-1],word[j]);
-            strcpy(word[j],x);
+     for(i=0;i<n;i++)
+        if(word[i][0]>=97){
+            nub[i]=word[i][0]-32;
             }
-        }
+        else {nub[i]=word[i][0];
+                }
+    for(i=1;i<n;i++)
+        for(j=1;j<n;j++)
+                 if(nub[j]<nub[j-1]){
+                    strcpy(x,word[j-1]);
+                    strcpy(word[j-1],word[j]);
+                    strcpy(word[j],x);
+                    k=nub[j-1];
+                    nub[j-1]=nub[j];
+                    nub[j]=k;
+                    }
     for(i=0;i<n;i++){
         printf("%s\n",word[i]);
         }
