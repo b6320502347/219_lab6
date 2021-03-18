6652be3a475e153555a9d7fff41e65760ed4443c
b6320502347 <78533381+b6320502347@users.noreply.github.com>
2021-03-18 15:28:10 +0700

    .

--- 6320502347_6_1.c
+++ 6320502347_6_1.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 #include<string.h>
     int main(){
-    int n,i;
+    int n,i,j;
     scanf("%d",&n);
     char word[n][1000],x[1000];
     for(i=0;i<n;i++){
         scanf("%s",word[i]);
         fflush(stdin);
         }
-    for(i=1;i<n;i++){
-        if(word[i][0]<word[i-1][0]){
-            strcpy(x,word[i-1]);
-            strcpy(word[i-1],word[i]);
-            strcpy(word[i],x);
+    for(i=1;i<n;i++)
+    for(j=1;j<n;j++){
+        if(word[j][0]<word[j-1][0]){
+            strcpy(x,word[j-1]);
+            strcpy(word[j-1],word[j]);
+            strcpy(word[j],x);
             }
         }
     for(i=0;i<n;i++){
